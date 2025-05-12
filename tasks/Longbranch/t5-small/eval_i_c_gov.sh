03ee01e0aba895bb93587e9ce642d7625c2aa134
HT-NEKO <774114904@qq.com>
2025-05-12 17:04:17 +0800

    add tasks

---
+++ tasks/Longbranch/t5-small/eval_i_c_gov.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+module load miniforge gcc/11.1.0 cuda/11.8 
+module load cudnn/8.6.0_cuda11.x
+source activate bart-small
+export PYTHONUNBUFFERED=1
+export num_gpu=4
+DATASET=GovReport
+CHECKPOINT=/data/home/scv6872/run/kwli/infoscale/text-to-text-transfer-transformer-main/BART-SMALL/t5_checkpoint/GovReport/CosScale-256/checkpoint-3285
+for SEQ_LEN in 1000 2000 3000
+do
+    # torchrun --nproc_per_node=${num_gpu} --master_port=44000 
+    python run_summarization.py \
+    --model_name_or_path $CHECKPOINT \
+    --do_predict \
+    --dataset_name $DATASET \
+    --dataset_config_name "3.0.0" \
+    --source_prefix "summarize: " \
+    --per_device_train_batch_size=4 \
+    --per_device_eval_batch_size=4 \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --kyw_normalization "InfoScale" \
+    --kyw_norm_method "CosScale" \
+    --kyw_norm_scale 256 \
+    --max_source_length $SEQ_LEN
+done
